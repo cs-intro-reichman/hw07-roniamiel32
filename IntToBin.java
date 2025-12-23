87ff6df1c8aa29b52789ae70f443e34570fc4032
Roni Amiel <roniamiel@Ronis-MacBook-Air.local>
2025-12-23 22:22:11 +0200

    done q1

--- IntToBin.java
+++ IntToBin.java
@@ -9,7 +9,7 @@ public class IntToBin {
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
         if (x == 0 || x == 1) return "" + x;  
-        if (x % 2 == 0) return "0" + toBinary(x/2);
-        else return "1" + toBinary(x/2);
+        if (x % 2 == 0) return  toBinary(x/2) + "0";
+        else return  toBinary(x/2) + "1";
     } 
  }
